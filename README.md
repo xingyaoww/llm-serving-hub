03239e6b738b0d3aea9db26e2238d444d57b1310
Xingyao Wang <xingyao6@illinois.edu>
2023-12-22 12:42:54 +0000

    support tgi

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 # Install litellm
 conda create -n llm-serve python=3.10
 conda activate llm-serve
+pip install vllm
 pip install 'litellm[proxy]'
 ```
 
@@ -26,4 +27,7 @@ source source.sh
 # MODEL_PATH, MAX_INPUT_LENGTH, MAX_TOTAL_TOKENS (need for TGI)
 tgi_serve_model_docker /data/shared/Llama-2-70b-chat-hf 3968 4096
 tgi_serve_model_docker /data/shared/CodeLlama-34b-Instruct-hf 16256 16384
+
+# MODEL_PATH, MAX_TOTAL_TOKENS, CHAT_TEMPLATE
+vllm_serve_model_docker /data/shared/CodeLlama-34b-Instruct-hf/ 16384 chat_templates/llama.jinja
 ```
--- source.sh
+++ source.sh
@@ -26,3 +26,46 @@ tgi_serve_model_docker() {
     
     echo "Model $MODEL_NAME is served with TGI using GPU $CUDA_VISIBLE_DEVICES on http://localhost:8080"
 }
+
+
+vllm_serve_model_docker() {
+    FULL_PATH=$1
+    MAX_TOTAL_TOKENS=$2
+    CHAT_TEMPLATE=$3
+    MODEL_DIR=$(dirname "$FULL_PATH")
+    MODEL_NAME=$(basename "$FULL_PATH")
+    echo "Serving model $MODEL_NAME from $MODEL_DIR"
+
+    docker run \
+        -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+        --runtime nvidia --gpus all \
+        -v $MODEL_DIR:/data \
+        -v `pwd`:/workspace \
+        -p 8000:8000 \
+        --ipc=host \
+        vllm/vllm-openai:latest \
+        --model /data/$MODEL_NAME \
+        --served-model-name $MODEL_NAME \
+        --max-num-batched-tokens $MAX_TOTAL_TOKENS \
+        --chat-template /workspace/$CHAT_TEMPLATE \
+
+    echo "Model $MODEL_NAME is served with vLLM using GPU $CUDA_VISIBLE_DEVICES on http://localhost:8080"
+}
+
+
+vllm_serve_model() {
+    FULL_PATH=$1
+    MAX_TOTAL_TOKENS=$2
+    CHAT_TEMPLATE=$3
+    MODEL_DIR=$(dirname "$FULL_PATH")
+    MODEL_NAME=$(basename "$FULL_PATH")
+    echo "Serving model $MODEL_NAME from $MODEL_DIR"
+
+    python3 -m vllm.entrypoints.openai.api_server \
+        --model $FULL_PATH \
+        --served-model-name $MODEL_NAME \
+        --max-num-batched-tokens $MAX_TOTAL_TOKENS \
+        --chat-template $CHAT_TEMPLATE \
+
+    echo "Model $MODEL_NAME is served with vLLM using GPU $CUDA_VISIBLE_DEVICES on http://localhost:8080"
+}
